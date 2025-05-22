a3f16ad0e1479150810e762a25740b2e8dca9115
Marius Troy <marius@ohmy.no>
2025-05-22 14:48:44 +0200

    removed guru pro btn login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -77,9 +77,3 @@ if (error) {                 // ← NEW
     </form>
   );
 }
- <p className="text-center text-sm mt-4 text-gray-400">
-  Want full access?{" "}
-  <a href="/subscribe" className="text-cyan-500 hover:underline">
-    Get Guru Pro
-  </a>
-</p>
