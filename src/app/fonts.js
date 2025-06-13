23274c60307aed579b37d957f0ad0c9e6562349c
Marius Troy <marius@ohmy.no>
2025-06-13 12:47:50 +0200

    new font file

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,14 +1,32 @@
-// app/fonts.js
-import localFont from 'next/font/local';
+// src/app/fonts.js
+import localFont from "next/font/local";
 
 export const elanor = localFont({
   src: [
-	{ path: '../public/fonts/Elanor/Elanor-Regular.woff2', weight: '700' },
-	{ path: '../public/fonts/Elanor/Elanor-Regular.woff',  weight: '700' },
-	{ path: '../public/fonts/Elanor/Elanor-ExtraLight.woff2', weight: '400' },
-	{ path: '../public/fonts/Elanor/Elanor-ExtraLight.woff',  weight: '400' },
-	…
+	/* ExtraLight – weight 300 (adjust if your design uses 200) */
+	{
+	  path: "../../public/fonts/Elanor/Elanor-ExtraLight.woff2",
+	  weight: "300",
+	  style: "normal",
+	},
+	{
+	  path: "../../public/fonts/Elanor/Elanor-ExtraLight.woff",
+	  weight: "300",
+	  style: "normal",
+	},
+
+	/* Regular – weight 400 */
+	{
+	  path: "../../public/fonts/Elanor/Elanor-Regular.woff2",
+	  weight: "400",
+	  style: "normal",
+	},
+	{
+	  path: "../../public/fonts/Elanor/Elanor-Regular.woff",
+	  weight: "400",
+	  style: "normal",
+	},
   ],
-  display: 'swap',
-  variable: '--font-elanor',
+  display: "swap",          // avoids flash of invisible text
+  variable: "--font-elanor" // makes Tailwind utility `font-elanor` work
 });
